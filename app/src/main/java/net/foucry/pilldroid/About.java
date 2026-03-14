513820133e5553f4ef3910987ef5cb34383b5ba7
Jean-Baptiste <jeanbaptiste.charron@outlook.fr>
2026-03-14 18:07:23 +0100

    Add a new button on about screen

--- app/src/main/java/net/foucry/pilldroid/About.java
+++ app/src/main/java/net/foucry/pilldroid/About.java
@@ -1,10 +1,13 @@
 package net.foucry.pilldroid;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.button.MaterialButton;
 
 public class About extends AppCompatActivity {
 
@@ -15,6 +18,11 @@ public class About extends AppCompatActivity {
         MaterialToolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
+        MaterialButton code = findViewById(R.id.source_code);
+        code.setOnClickListener( v -> {
+            String url = "https://github.com/Jean-BaptisteC/Pilldroid";
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+        });
     }
 }
